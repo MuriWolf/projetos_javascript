6b394b52b078ab81d64fda9fad0389b9a94fe270
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-04-07 22:37:52 -0300

    review challenge

--- color_flipper/hex.js
+++ color_flipper/hex.js
@@ -1,4 +1,5 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+             "A", "B", "C", "D", "E", "F"]
 
 const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
@@ -7,7 +8,6 @@ btn.addEventListener("click", function () {
     let hexColor = "#"
     for (let i=0; i<6; i++) {
         hexColor += hex[getRandomNumber()]
-
     }
     document.body.style.backgroundColor = hexColor
     color.textContent = hexColor
